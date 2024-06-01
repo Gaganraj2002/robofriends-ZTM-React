e98b2dd52788c80afc040d3e867c593ecb58e8b0
Gagan Raj <gaganraj2002@gmail.com>
2024-06-02 02:53:17 +0530

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import CardList from "../components/CardList";
+
+import SearchBox from "../components/SearchBox";
+import { Component } from "react";
+import "./App.css";
+import Scroll from "../components/Scroll";
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchfield: ""
+        };
+        this.setState({robots: fetch("https://jsonplaceholder.typicode.com/users")});
+    }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+    }
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        if (this.state.robots.length === 0) {
+            return (
+                <div>   
+                <h1>RoboFriends</h1>
+            <h1>Loading...</h1>;
+                </div>
+            )
+        }
+
+        else{
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+
+                <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }}
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,122 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Rishi',
+        username: 'Rishi',
+        email: 'rishii@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Rishabh',
+        username: 'Rishabh',
+        email: "Rishabh@gmail.com"
+    },
+    {
+        id: 3,
+        name: 'John',
+        username: 'johnny',
+        email: 'johnny@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Jane',
+        username: 'jane_doe',
+        email: 'jane_doe@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Alex',
+        username: 'alex123',
+        email: 'alex123@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Sarah',
+        username: 'sarah_89',
+        email: 'sarah_89@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Michael',
+        username: 'mike_m',
+        email: 'mike_m@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Emily',
+        username: 'emily_rose',
+        email: 'emily_rose@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'David',
+        username: 'david_123',
+        email: 'david_123@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Olivia',
+        username: 'olivia_w',
+        email: 'olivia_w@gmail.com'
+    },
+    {
+        id: 11,
+        name: 'Daniel',
+        username: 'daniel_89',
+        email: 'daniel_89@gmail.com'
+    },
+    {
+        id: 12,
+        name: 'Sophia',
+        username: 'sophia_m',
+        email: 'sophia_m@gmail.com'
+    },
+    {
+        id: 13,
+        name: 'Matthew',
+        username: 'matt_123',
+        email: 'matt_123@gmail.com'
+    },
+    {
+        id: 14,
+        name: 'Ava',
+        username: 'ava_w',
+        email: 'ava_w@gmail.com'
+    },
+    {
+        id: 15,
+        name: 'Andrew',
+        username: 'andrew_89',
+        email: 'andrew_89@gmail.com'
+    },
+    {
+        id: 16,
+        name: 'Emma',
+        username: 'emma_m',
+        email: 'emma_m@gmail.com'
+    },
+    {
+        id: 17,
+        name: 'Christopher',
+        username: 'chris_123',
+        email: 'chris_123@gmail.com'
+    },
+    {
+        id: 18,
+        name: 'Isabella',
+        username: 'isabella_w',
+        email: 'isabella_w@gmail.com'
+    },
+    {
+        id: 19,
+        name: 'Joshua',
+        username: 'josh_89',
+        email: 'josh_89@gmail.com'
+    },
+    {
+        id: 20,
+        name: 'Mia',
+        username: 'mia_m',
+        email: 'mia_m@gmail.com'
+    }
+];
